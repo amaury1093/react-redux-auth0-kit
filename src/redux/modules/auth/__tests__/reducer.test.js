abeb205b6a090700709d11c35b4762dc8d298946
Amaury Martiny <amaury.martiny@gmail.com>
2018-01-15 22:24:39 +0100

    Fix all tests

--- src/redux/modules/auth/__tests__/reducer.test.js
+++ src/redux/modules/auth/__tests__/reducer.test.js
@@ -1,52 +1,65 @@
-import { expect } from 'chai';
-
 import authReducer, { authTypes } from '../';
 
 require('jest-localstorage-mock'); // Provides a fake window.localStorage
 
 describe('(Reducer) auth', () => {
   it('should return the initial state', () => {
-    expect(authReducer(undefined, {})).to.deep.equal({
+    expect(authReducer(undefined, {})).toEqual({
       isAuthenticated: false,
       isFetching: false,
       profile: {},
-      error: null,
+      error: null
     });
   });
 
   it('should handle LOGIN_REQUEST', () => {
-    expect(authReducer(undefined, { type: authTypes.LOGIN_REQUEST })).to.deep.equal({
+    expect(authReducer(undefined, { type: authTypes.LOGIN_REQUEST })).toEqual({
       isAuthenticated: false,
       isFetching: true,
       profile: {},
-      error: null,
+      error: null
     });
   });
 
   it('should handle LOGIN_SUCCESS', () => {
-    expect(authReducer(undefined, { type: authTypes.LOGIN_SUCCESS, profile: { fakeProperty: 'fakeValue' } })).to.deep.equal({
+    expect(
+      authReducer(undefined, {
+        type: authTypes.LOGIN_SUCCESS,
+        payload: { profile: { fakeProperty: 'fakeValue' } }
+      })
+    ).toEqual({
       isAuthenticated: true,
       isFetching: false,
       profile: { fakeProperty: 'fakeValue' },
-      error: null,
+      error: null
     });
   });
 
   it('should handle LOGIN_ERROR', () => {
-    expect(authReducer(undefined, { type: authTypes.LOGIN_ERROR, error: 'fakeError' })).to.deep.equal({
+    expect(
+      authReducer(undefined, {
+        type: authTypes.LOGIN_ERROR,
+        error: 'fakeError'
+      })
+    ).toEqual({
       isAuthenticated: false,
       isFetching: false,
       profile: {},
-      error: 'fakeError',
+      error: 'fakeError'
     });
   });
 
   it('should handle LOGOUT_SUCCESS', () => {
-    expect(authReducer(undefined, { type: authTypes.LOGOUT_SUCCESS, error: 'fakeError' })).to.deep.equal({
+    expect(
+      authReducer(undefined, {
+        type: authTypes.LOGOUT_SUCCESS,
+        error: 'fakeError'
+      })
+    ).toEqual({
       isAuthenticated: false,
       isFetching: false,
       profile: {},
-      error: null,
+      error: null
     });
   });
 });
